53fca0f128b43095e9d428ff87f9f2d781946c25
LahiruHarshana <lharshana2002@gmail.com>
2024-03-25 10:09:15 +0530

    updated

--- src/main/java/lk/ijse/spring/service/CustomerService.java
+++ src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -12,4 +12,9 @@ import java.util.List;
  **/
 public interface CustomerService {
     List<CustomerDTO> getAllCustomers();
+    CustomerDTO getCustomerDetails(String id);
+    void saveCustomer(CustomerDTO customerDTO);
+    void updateCustomer(CustomerDTO customerDTO);
+    void deleteCustomer(String id);
+
 }
---
+++ src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -0,0 +1,56 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.CustomerDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : L.H.J
+ * @File: CustomerServiceImpl
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-25, Monday
+ **/
+public class CustomerServiceImpl implements CustomerService{
+
+    ArrayList<CustomerDTO> customerList = new ArrayList<>();
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return customerList;
+    }
+
+    @Override
+    public CustomerDTO getCustomerDetails(String id) {
+        for(CustomerDTO customerDTO : customerList){
+            if(customerDTO.getId().equals(id)){
+                return customerDTO;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+        customerList.add(customerDTO);
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO customerDTO) {
+        for(CustomerDTO customer : customerList){
+            if(customer.getId().equals(customerDTO.getId())){
+                customerList.set(customerList.indexOf(customer),customerDTO);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+        for(CustomerDTO customer : customerList){
+            if(customer.getId().equals(id)){
+                customerList.remove(customer);
+                break;
+            }
+        }
+    }
+}
