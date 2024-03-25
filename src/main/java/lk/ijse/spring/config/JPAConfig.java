6c4ea6ea6248aa3930a5a5ecb00ae5e2264bf944
LahiruHarshana <lharshana2002@gmail.com>
2024-03-25 14:45:56 +0530

    updated

--- src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,11 +2,15 @@ package lk.ijse.spring.config;
 
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import javax.sql.DataSource;
 
 /**
@@ -15,6 +19,10 @@ import javax.sql.DataSource;
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-03-25, Monday
  **/
+
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repositories")
+@Configuration
 public class JPAConfig {
     @Bean
     public DataSource dataSource() {
---
+++ src/main/java/lk/ijse/spring/repositories/CustomerRepo.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.repositories;
+
+import lk.ijse.spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : L.H.J
+ * @File: CustomerRepo
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-25, Monday
+ **/
+public interface CustomerRepo extends JpaRepository<Customer,String> {
+}
