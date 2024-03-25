847cc70dd3fbf822ab5da096927a3a7183bab9e8
LahiruHarshana <lharshana2002@gmail.com>
2024-03-25 12:36:38 +0530

    updated

--- src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
+
 /**
  * @author : L.H.J
  * @File: JpaConfig
@@ -7,4 +9,31 @@ package lk.ijse.spring.config;
  * @created : 2024-03-25, Monday
  **/
 public class JPAConfig {
+    @Bean
+    public DataSource dataSource() {
+
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.acme.domain");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
 }
