447485af398cf795bd07a0a7596124b9b657fcc4
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 13:30:16 +0530

    added a validation for customer service

---
+++ src/main/java/lk/ijse/spring/service/exception/DuplicateRecordException.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.service.exception;
+
+/**
+ * @author : L.H.J
+ * @File: DuplicateRecordException
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-28, Thursday
+ **/
+public class DuplicateRecordException {
+}
