b2fcb945165fa4b655e4598e297735a678c7d270
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 13:55:39 +0530

    updated

---
+++ src/main/java/lk/ijse/spring/advice/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.advice;
+
+/**
+ * @author : L.H.J
+ * @File: GlobalExceptionHandler
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-28, Thursday
+ **/
+public class GlobalExceptionHandler {
+}
