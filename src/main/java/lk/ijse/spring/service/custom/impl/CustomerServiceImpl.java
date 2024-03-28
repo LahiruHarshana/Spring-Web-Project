66d3822ad75ae217f4198051ef5e3b9243c3c3bd
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 13:38:29 +0530

    updated

--- src/main/java/lk/ijse/spring/service/custom/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/custom/impl/CustomerServiceImpl.java
@@ -4,6 +4,8 @@ import jakarta.transaction.Transactional;
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.repositories.CustomerRepo;
 import lk.ijse.spring.service.custom.CustomerService;
+import lk.ijse.spring.service.exception.DuplicateRecordException;
+import lk.ijse.spring.service.exception.NotFoundException;
 import lk.ijse.spring.service.util.Transformer;
 import lk.ijse.spring.service.util.UtilMatter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,13 +40,16 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDTO getCustomerDetails(String id) {
         if (!customerRepo.existsById(id)){
-            throw new RuntimeException("No customer exist for the provided id");
+            throw new NotFoundException("No customer exist for the provided id");
         }
         return transformer.fromCustomerEntity(customerRepo.findById(id).get());
     }
 
     @Override
     public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+        if (customerRepo.existsById(customerDTO.getCus_id())){
+            throw new DuplicateRecordException("Customer already exist for the provided id");
+        }
         customerDTO.setCus_id(UtilMatter.generateID());
         return transformer.fromCustomerEntity(customerRepo.save(transformer.toCustomerEntity(customerDTO)));
     }
@@ -52,7 +57,7 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void updateCustomer(CustomerDTO customerDTO) {
         if (!customerRepo.existsById(customerDTO.getCus_id())){
-            throw new RuntimeException("No customer exist for the provided id");
+            throw new NotFoundException("No customer exist for the provided id");
         }
         customerRepo.save(transformer.toCustomerEntity(customerDTO));
     }
@@ -60,7 +65,7 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void deleteCustomer(String id) {
         if (!customerRepo.existsById(id)){
-            throw new RuntimeException("No customer exist for the provided id");
+            throw new NotFoundException("No customer exist for the provided id");
         }
         customerRepo.deleteById(id);
     }
