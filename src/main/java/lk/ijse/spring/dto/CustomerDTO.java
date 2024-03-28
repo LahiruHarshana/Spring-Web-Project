f71291925b17c17c8697a2dd92ac02de27eb0842
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 11:53:46 +0530

    added a validation for customer service

--- src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,6 +1,10 @@
 package lk.ijse.spring.dto;
 
 import java.io.Serializable;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 /**
  * @author : L.H.J
@@ -14,9 +18,14 @@ import lombok.*;
 @NoArgsConstructor
 @Data
 public class CustomerDTO implements Serializable {
+    @Null(message = "Customer ID must be null")
     private String cus_id;
+    @NotBlank(message = "Name can not be null")
+    @Pattern(regexp = "^[A-Za-z ]*$", message = "Name should only contain alphabetical characters")
     private String name;
+    @NotBlank(message = "Address can not be null")
     private String address;
+    @NotBlank(message = "Salary can not be null")
     private double salary;
 
 }
