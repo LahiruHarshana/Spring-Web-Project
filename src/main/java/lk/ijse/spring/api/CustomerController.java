88d9374c4d1aa63967066607265dc5b6244b66de
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 11:57:18 +0530

    added a validation for customer service

--- src/main/java/lk/ijse/spring/api/CustomerController.java
+++ src/main/java/lk/ijse/spring/api/CustomerController.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.api;
 
+import jakarta.validation.Valid;
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.service.custom.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +25,13 @@ public class CustomerController {
 
         @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
         @ResponseStatus(HttpStatus.CREATED)
-        public void saveCustomer(@RequestBody CustomerDTO customer){
+        public void saveCustomer(@Valid @RequestBody CustomerDTO customer){
             customerService.saveCustomer(customer);
 
         }
 
         @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-        public void updateCustomer(@RequestBody CustomerDTO customer){
+        public void updateCustomer( @Valid @RequestBody CustomerDTO customer){
             System.out.println(customer.getName());
              customerService.updateCustomer(customer);
         }
