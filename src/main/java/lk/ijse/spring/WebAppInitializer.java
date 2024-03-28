dd65bc3b12e0f7207799fc9854384f2c09b19ab1
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 14:47:50 +0530

    updated

--- src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,5 +1,7 @@
-package lk.ijse.spring.config;
+package lk.ijse.spring;
 
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
