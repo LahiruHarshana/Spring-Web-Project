4aac59ebb52505ad881dac05cac73b4ec3a5fd4c
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 09:37:23 +0530

    couse updated

--- src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -11,7 +11,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
 
 /**
