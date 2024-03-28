da1c2844bb86ad3b1732d884dbbc698a7e453bc4
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 13:59:02 +0530

    updated

--- src/main/java/lk/ijse/spring/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/spring/advice/GlobalExceptionHandler.java
@@ -1,6 +1,12 @@
 package lk.ijse.spring.advice;
 
+import lk.ijse.spring.service.exception.ServiceException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author : L.H.J
@@ -11,4 +17,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<Map<String, Objects>> handleServiceException(ServiceException ex){
+        return null;
+    }
 }
