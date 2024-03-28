3f1e73a920105535d3af4d84599b1b82c75e2c3c
LahiruHarshana <lharshana2002@gmail.com>
2024-03-28 13:33:35 +0530

    updated

--- src/main/java/lk/ijse/spring/service/exception/DuplicateRecordException.java
+++ src/main/java/lk/ijse/spring/service/exception/DuplicateRecordException.java
@@ -6,5 +6,5 @@ package lk.ijse.spring.service.exception;
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-03-28, Thursday
  **/
-public class DuplicateRecordException {
+public class DuplicateRecordException extends ServiceException{
 }
